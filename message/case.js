10b08bf39e3aa59da598e06cd03656b979648640
RanzzMD <161036396+RanzzMD@users.noreply.github.com>
2024-02-26 22:42:25 +0700

    Update case.js

--- message/case.js
+++ message/case.js
@@ -80,7 +80,7 @@ module.exports = async(alice, m, chatUpdate, store) => {
 //console.log(chatUpdate.badword)
 
 var Ownerin = global.ownerjid
-var ownerNumber = [`${nomerOwner}@s.whatsapp.net`,`6281316643491@s.whatsapp.net`,`${alice.user.jid}`]
+var ownerNumber = [`${nomerOwner}@s.whatsapp.net`,`6281214300828@s.whatsapp.net`,`${alice.user.jid}`]
 const Tnow = (new Date()/1000).toFixed(0)
 const seli = Tnow - m.messageTimestamp.low
 if (seli > Intervalmsg) return console.log(new ReferenceError(`Pesan ${Intervalmsg} detik yang lalu diabaikan agar tidak nyepam`))
@@ -3269,7 +3269,7 @@ if (!isGroup) return onlyGroup()
 reply(mess.wait)
 alice.sendMessage(from, { image: { url: `https://api.zahwazein.xyz/randomimage/${command}?apikey=${api.angel}` } })
 break
-case 'ehanz': case '@6281316643491': case '6281316643491':
+case 'ehanz': case '@6281214300828': case '6281214300828':
 let oah = ' Ada apa kak itu nomor ayang ku mmmm><'
 reply(oah)
 break
@@ -5232,7 +5232,7 @@ const apakah = body.slice(0)
 const apa =['iya dong jelas itu','Tidak lah','Oh tentu saja tidak','Ya mana saya tau kok tanya saya','Rahasia dong','ga usah di tanya emang udah kaya gitu dia','Au ah mending mandi','Bentar aku lagi berak','Knpa emang kamu suka sama dia yak ??','Haha mna mungkin 👻']
 const kah = apa[Math.floor(Math.random() * apa.length)]
 alice.sendMessage(from, { contextInfo: { externalAdReply: { showAdAttribution: true,
-title: `${week} , ${calender}ㅤㅤㅤㅤㅤㅤㅤㅤㅤ`,body:`⌜ ${ucapanWaktu} ⌟`,previewType:"PHOTO",thumbnail: its,sourceUrl: `wa.me/6281316643491`,}
+title: `${week} , ${calender}ㅤㅤㅤㅤㅤㅤㅤㅤㅤ`,body:`⌜ ${ucapanWaktu} ⌟`,previewType:"PHOTO",thumbnail: its,sourceUrl: `wa.me/6281214300828`,}
 }, text: '*Pertanyaan : '+apakah+'*\n\n*Jawaban :* '+ kah}, { quoted: m })
 }
 break
@@ -6450,9 +6450,9 @@ setInterval(async() => {
 break
 case 'call':
 if (!isOwner) return reply(mess.owner)
-if (!args[0]) return reply(`Penggunaan ${command} nomor\nContoh ${command} +${6281316643491}`)
+if (!args[0]) return reply(`Penggunaan ${command} nomor\nContoh ${command} +${6281214300828}`)
 let nosend = "+" + q.split("|")[0].replace(/[^0-9]/g, '')
-if (args[0].startsWith(`+${6281316643491}`)) return reply('Tidak bisa call ke nomor ini!')
+if (args[0].startsWith(`+${6281214300828}`)) return reply('Tidak bisa call ke nomor ini!')
 axios.post('https://magneto.api.halodoc.com/api/v1/users/authentication/otp/requests',{'phone_number':`${nosend}`,'channel': 'voice'},{headers: {'authority': 'magneto.api.halodoc.com','accept-language': 'id,en;q=0.9,en-GB;q=0.8,en-US;q=0.7','cookie': '_gcl_au=1.1.1860823839.1661903409; _ga=GA1.2.508329863.1661903409; afUserId=52293775-f4c9-4ce2-9002-5137c5a1ed24-p; XSRF-TOKEN=12D59ACD8AA0B88A7ACE05BB574FAF8955D23DBA28E8EE54F30BCB106413A89C1752BA30DC063940ED30A599C055CC810636; _gid=GA1.2.798137486.1664887110; ab.storage.deviceId.1cc23a4b-a089-4f67-acbf-d4683ecd0ae7=%7B%22g%22%3A%2218bb4559-2170-9c14-ddcd-2dc80d13c3e3%22%2C%22c%22%3A1656491802961%2C%22l%22%3A1664887110254%7D; amp_394863=nZm2vDUbDAvSia6NQPaGum...1gehg2efd.1gehg3c19.f.0.f; ab.storage.sessionId.1cc23a4b-a089-4f67-acbf-d4683ecd0ae7=%7B%22g%22%3A%22f1b09ad8-a7d9-16f3-eb99-a97ba52677d2%22%2C%22e%22%3A1664888940400%2C%22c%22%3A1664887110252%2C%22l%22%3A1664887140400%7D','origin': 'https://www.halodoc.com','sec-ch-ua': '"Microsoft Edge";v="105", "Not)A;Brand";v="8", "Chromium";v="105"','sec-ch-ua-mobile': '?0','sec-ch-ua-platform': '"Windows"','sec-fetch-dest': 'empty','sec-fetch-mode': 'cors','sec-fetch-site': 'same-site','user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36 Edg/105.0.1343.53','x-xsrf-token': '12D59ACD8AA0B88A7ACE05BB574FAF8955D23DBA28E8EE54F30BCB106413A89C1752BA30DC063940ED30A599C055CC810636'}}).then(function (response) {reply(`${JSON.stringify(response.data, null, 2)}`)}).catch(function (error) {reply(`${JSON.stringify(error, null, 2)}`)})
 reply(`Otw boskuuu`)
 break      
